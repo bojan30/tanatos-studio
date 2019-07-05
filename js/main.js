8185937c5009ac8c7d0f1e61aa160698203798b3
bojan <bojancuk33@gmail.com>
2019-07-05 23:51:29 +0200

    side menu functionality added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 const teamMembers = document.querySelectorAll(".team-member");
 const teamMembersArray = Array.from(teamMembers);
 const dots = document.querySelectorAll('.dots');
+const menuButton = document.querySelector(".menu");
+const sideMenu = document.querySelector(".side-menu");
+const closeSideMenuOverlay = document.querySelector(".side-menu-overlay");
+const times = document.querySelector(".close");
 
 //listen for a mouseover and add active class to a dot element with the same index
 teamMembersArray.forEach((member,index)=>{
@@ -22,3 +26,18 @@ function removeActiveClass(elements){
     el.classList.remove("dot-active");
   })
 }
+
+menuButton.addEventListener('click', openSideMenu);
+
+function openSideMenu(){
+  closeSideMenuOverlay.classList.add("overlay-active");
+  sideMenu.classList.add("side-menu-active");
+}
+
+closeSideMenuOverlay.addEventListener("click",closeSideMenu);
+
+function closeSideMenu(){
+  sideMenu.classList.remove("side-menu-active");
+  closeSideMenuOverlay.classList.remove("overlay-active")
+}
+times.addEventListener("click",closeSideMenu);
